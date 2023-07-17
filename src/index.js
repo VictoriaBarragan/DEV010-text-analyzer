304af6ac0b28b70276dfe382ce18dcadfa324247
VictoriaBarragan <elsavictoriabarragan@gmail.com>
2023-07-17 18:38:28 -0500

    checkpoint commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
+
+function clear_txtBox(){
+document.getElementsByName("user-input")[0].value = "";
+}

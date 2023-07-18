118d29121fe965c0f99ddbbc493baae0138731c0
VictoriaBarragan <elsavictoriabarragan@gmail.com>
2023-07-18 18:53:39 -0500

    Prototipo alta fidelidad

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
 
-function clear_txtBox(){
-document.getElementsByName("user-input")[0].value = "";
-}
+function limpiarAreaTxt(){
+    document.getElementsByTagName("textarea")[0].value = "";
+};
+

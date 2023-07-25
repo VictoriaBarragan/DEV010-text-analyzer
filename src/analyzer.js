ea95dadfa9c811c12f05b24528d0d2d1f7d5caa0
VictoriaBarragan <elsavictoriabarragan@gmail.com>
2023-07-24 21:43:13 -0500

    Metricas

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,10 +1,25 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount:(text)=>{
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let arr=text.split(' ');
+    let cantPalabras=0;
+    for(let i=0; i<arr.length; i++){
+      cantPalabras+=1;
+    }
+    
+    return cantPalabras;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let arrSinEspacios=text.trim();
+    let cantCaracteres=0;
+    for(let i=0; i<arrSinEspacios.length; i++){
+      cantCaracteres+=1;
+    }
+    return cantCaracteres;
   },
+  
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
-
+let btnLimpiar = addEventListener("click", limpiarAreaTxt);
 function limpiarAreaTxt(){
     document.getElementsByTagName("textarea")[0].value = "";
 };
+
+let arregloTxt = document.getElementsByName("user-input");
+arregloTxt[0].addEventListener("keyup", ()=>{
+    let txt=arregloTxt[0].value; 
+    let contadorDePalabras = analyzer.getWordCount(txt);
+    let contadorDeCaracteres = analyzer.getCharacterCount(txt);
+    document.querySelector('li[data-testid="word-count"]').textContent = "Palabras " + contadorDePalabras;
+    document.querySelector('li[data-testid="character-count"]').textContent = "Caracteres " + contadorDeCaracteres;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
